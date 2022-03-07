e22434bb40ef6bd83108f5173d965b0b033edcc4
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2022-03-06 21:09:52 -0700

    enviando token con interceptores

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators'
+
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +12,8 @@ export class AuthService {
 
   constructor(
     private af: AngularFireAuth,
-    private http: HttpClient
+    private http: HttpClient,
+    private tokenService: TokenService
   ) { }
 
   createUser(email: string, password: string) {
@@ -32,6 +36,11 @@ export class AuthService {
     return this.http.post('https://platzi-store.herokuapp.com/auth', {
       email,
       password
-    })
+    }).pipe(
+      tap((data: {token: string}) => {
+        const token = data.token;
+        this.tokenService.saveToken()
+      })
+    )
   }
 }
